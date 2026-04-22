a2c93655503b127578e366ab429d2190009ddd9c
Dominik015 <158041727+Dominik015@users.noreply.github.com>
2026-04-22 14:42:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let range = document.getElementById("border_width");
+let text = document.getElementById("px");
+
+function RefreshPX(){
+text.textContent = range.value+" px";
+
+
+}
+
+
+function AddTable(){
+    
+
+
+}
